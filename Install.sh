f8c0bc695dcf94004fc60b4a2d8a8c207dc38926
ddtoma <ll66456645@163.com>
2015-09-03 11:58:31 +0800

    auto install missing dependence(shell).

--- Install.sh
+++ Install.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-echo '' > no_dp
+function print_help(){
+    cat << END_OF_HELP
+用法：
+  ./install.sh [选项]...
+选项：
+  -h, --help                  显示帮助信息
+  -c, --check-dependence      检查基本依赖
+  -i, --install               安装配置文件
+  -d, --install-dependence    安装依赖环境
+示例：
+  ./install.sh -c -i -d
+END_OF_HELP
+    exit 0
+}
+if [ $# -eq 0 ];then
+    print_help
+fi
 
 case $1 in
     -c|--check-dependence)
+        echo '' > no_dp
         echo 'checking dependence now......'
         . ./dependence.sh
         for path in $(pwd)/* ; do
@@ -33,20 +50,15 @@ case $1 in
         done
         ;;
     -d|--install-dependence)
+        if [[ -n `cat $HOME/Profiles/no_dp` ]]; then
+            echo "please install dependences"
+            sudo pacman -S $(cat $HOME/Profiles/no_dp)
+        else
+            echo "-----dependence is OK------"
+        fi
         ;;
     -h|--help)
-        cat << END_OF_HELP
-用法：
-  ./install.sh [选项]...
-选项：
-  -h, --help                  显示帮助信息
-  -c, --check-dependence      检查基本依赖
-  -i, --install               安装配置文件
-  -d, --install-dependence    安装依赖环境
-示例：
-  ./install.sh -c -i -d
-END_OF_HELP
-        exit 0
+        print_help
         ;;
     *)
         break
