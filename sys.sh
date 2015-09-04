8b0e51109bfe6e27e8f5e8d38bb429dffe4e738f
ddtoma <ll66456645@163.com>
2015-09-04 17:19:37 +0800

    change way about installing yaourt

--- sys.sh
+++ sys.sh
@@ -3,27 +3,12 @@
 /etc/pacman.d/mirrorlist < cat <<EOF
 Server = http://mirrors.aliyun.com/archlinux/$repo/os/$arch
 EOF
+/etc/pacman.conf << cat <<EOF
+[archlinuxfr]
+SigLevel = Never
+Server = http://repo.archlinux.fr/$arch
+EOF
 pacman -Syy
-pacman -S <(cat $HOME/Profiles/package_data)
-
-function insyaourt{
-    cd /tmp
-    echo 'install yaout'
-    git clone https://aur.archlinux.org/package-query.git
-    cd package-query
-    makepkg -si
-    pacman -U *.tar.xz
-    sleep 2
-
-    cd ..
-    git clone https://aur.archlinux.org/yaourt.git
-    cd yaourt
-    makepkg -si
-    cd ..
-    pacman -U *.tar.xz
-    echo 'fineshed'
-}
-insyaourt
 
 /etc/environment < cat <<EOF
 export LC_CTYPE=zh_CN.UTF-8
